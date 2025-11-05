9149674cb05f9d9804ba6a27c40c96d9b932d385
Mark Manders <mmjmanders@gmail.com>
2025-11-05 10:50:57 +0100

    Add margins (#5)
    * Reduced width
    
    * Updated favicons
    
    * Added Version component

---
+++ src/lib/components/Version.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { PUBLIC_BUILD_SHA as buildSha } from '$env/static/public';
+</script>
+
+<div
+	class="flex w-full justify-center font-[Kode_Mono_Variable] text-xs text-zinc-950 dark:text-zinc-50"
+>
+	{buildSha}
+</div>
