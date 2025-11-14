4a6e8c4ec426696434c1d5af2fda6a006d29f69f
Mark Manders <mmjmanders@gmail.com>
2025-11-14 14:02:00 +0100

    Preload fonts

--- src/lib/components/Version.svelte
+++ src/lib/components/Version.svelte
@@ -2,8 +2,6 @@
 	import { PUBLIC_BUILD_SHA as buildSha } from '$env/static/public';
 </script>
 
-<div
-	class="flex w-full justify-center font-[Kode_Mono_Variable] text-xs text-zinc-950 dark:text-zinc-50"
->
+<div class="flex w-full justify-center font-[Kode_Mono] text-xs text-zinc-950 dark:text-zinc-50">
 	{buildSha}
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
 	import '@fontsource/allerta-stencil';
-	import '@fontsource-variable/kode-mono';
+	import '@fontsource/kode-mono';
 	import '@fortawesome/fontawesome-free/css/solid.css';
 	import '../app.css';
 
+	import allertaStencilWoff2 from '@fontsource/allerta-stencil/files/allerta-stencil-latin-400-normal.woff2?url';
+	import kodeMonoWoff2 from '@fontsource/kode-mono/files/kode-mono-latin-400-normal.woff2?url';
+
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link
+		rel="preload"
+		as="font"
+		type="font/woff2"
+		href={allertaStencilWoff2}
+		crossorigin="anonymous"
+	/>
+	<link rel="preload" as="font" type="font/woff2" href={kodeMonoWoff2} crossorigin="anonymous" />
+</svelte:head>
 
 {@render children()}
